1c56b5c4e3a24fad028e1c73378584076f3fcbc5
Kumar Vishal <kumarvis@gmail.com>
2019-10-31 00:29:56 +0530

    thesis writing

--- thesis_exp/plot_lr_hidden_layer_bar.py
+++ thesis_exp/plot_lr_hidden_layer_bar.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 3
+
+hidden_layers_unit = [6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+N = len(hidden_layers_unit)
 ind = np.arange(N)  # the x locations for the groups
 width = 0.27       # the width of the bars
 
@@ -15,25 +17,25 @@ font = {'family': 'serif',
         }
 plt.text(0.01, 9, r'learning rate =0.1', fontdict=font)
 
-yvals = [4, 9, 2]
+yvals = [6+1, 8+1, 10+1, 12+1, 14+1, 16+1, 18+1, 20+1, 22+1, 24+1, 26+1, 28+1]
 rects1 = ax.bar(ind, yvals, width, color='r')
-zvals = [1, 2, 3]
+zvals = [6-1, 8-1, 10-1, 12-1, 14-1, 16-1, 18-1, 20-1, 22-1, 24-1, 26-1, 28-1]
 rects2 = ax.bar(ind+width, zvals, width, color='g')
 
 ax.set_ylabel('Accuracy')
 ax.set_xlabel('Number of Hidden Units')
 ax.set_xticks(ind+width)
-ax.set_xticklabels(('2011-Jan-4', '2011-Jan-5', '2011-Jan-6'))
+ax.set_xticklabels(tuple(hidden_layers_unit) )
 ax.legend((rects1[0], rects2[0]), ('Train', 'Validation'))
 
-def autolabel(rects):
-    for rect in rects:
-        h = rect.get_height()
-        ax.text(rect.get_x()+rect.get_width()/2., 1.05*h, '%d'%int(h),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
+# def autolabel(rects):
+#     for rect in rects:
+#         h = rect.get_height()
+#         ax.text(rect.get_x()+rect.get_width()/2., 1.05*h, '%d'%int(h),
+#                 ha='center', va='bottom')
+#
+# autolabel(rects1)
+# autolabel(rects2)
 
 
 plt.show()
